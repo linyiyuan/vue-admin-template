68b1fdc11cab212f673fdab8eb338b5a0c3bcf2e
linyiyuan <linyiyuann@163.com>
2020-03-04 10:07:08 +0800

    增加请求头

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,6 +15,7 @@ service.interceptors.request.use(config => {
   if (store.getters.token) {
     config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
   }
+  config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
   return config
 }, error => {
   // Do something with request error
